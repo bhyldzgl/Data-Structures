118151f5bcbb064ee08aaae8699a9ab9b7872df8
Baha Furkan Yıldızoğlu <129391111+bhyldzgl@users.noreply.github.com>
2024-01-16 18:49:23 +0300

    Add files via upload

---
+++ Binary Search Tree Add/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+      
+         BinarySearchTree binarySearchTree = new BinarySearchTree();
+         
+         binarySearchTree.add(30);
+         binarySearchTree.add(20);
+         binarySearchTree.add(15);
+         binarySearchTree.add(40);
+         binarySearchTree.add(50);
+         binarySearchTree.add(45);
+         binarySearchTree.add(38);
+         binarySearchTree.add(12);
+         binarySearchTree.add(18);
+         binarySearchTree.print();
+    }
+    
+}
---
+++ Binary Search Tree Add/src/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+
+  int data;
+  Node leftChild;
+  Node rightChild;
+  
+   public Node(int a){
+       
+       data = a;
+       rightChild = null;
+       leftChild = null;
+   }
+}
